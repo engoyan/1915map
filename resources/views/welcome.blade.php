190cff912a9799c72e049c33892ff97dd0379925
Vadim Engoyan <vadim@engoyan.com>
2015-04-21 13:41:53 -0700

    Add logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,33 @@
   <head>
     <style type="text/css">
       html, body, #map-canvas { height: 100%; width: 100%; margin: 0; padding: 0;}
+
+      #map-canvas{
+		  position: absolute;
+		  top: 0px;
+      }
+
+      .title{
+      	z-index: 1000;
+	  	position: absolute;
+  		font-family: 'Poiret One', cursive;
+		padding: 20px 40px;
+		text-align: center;
+		background-color: rgba(131, 131, 131, 0.3);
+		margin: 0 30px;
+	  }
+	  .title h1{
+	  	color: #F00;
+  		font-size: 34pt;
+  		margin-bottom: 0px;
+	  }
+	  .title span{
+  		font-size: 12pt;
+  		color:#FFF;
+	  }
+
     </style>
+    <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
     <script type="text/javascript"
       src="https://maps.googleapis.com/maps/api/js?v=3.exp">
     </script>
@@ -21,7 +47,7 @@
               streetViewControl: false,
               overviewMapControl: false,
               //38.8235622,42.2721864,7z
-            center: new google.maps.LatLng(38.8235622,42.2721864),
+            center: new google.maps.LatLng(39,42),
             styles: 
 
                     // [{"featureType":"all","elementType":"all","stylers":[{"invert_lightness":true},{"hue":"#435158"},{"saturation":10},{"lightness":30},{"gamma":0.5}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels.text","stylers":[{"color":"#7c95de"},{"weight":0.69},{"saturation":95},{"lightness":20}]}]         
@@ -35,6 +61,10 @@
     </script>
   </head>
   <body>
+  	<div class="title">
+	  	<h1>The 1915</h1>
+	  	<span>knowledge is priceless</span>
+  	</div>
     <div id="map-canvas"></div>
   </body>
 </html>
