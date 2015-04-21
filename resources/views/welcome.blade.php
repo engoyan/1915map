d9e3722efb8f681b6e5900ace6f976d339261e95
Vadim Engoyan <vadim@engoyan.com>
2015-04-21 11:31:47 -0700

    Adjust map

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,41 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+  <head>
+    <style type="text/css">
+      html, body, #map-canvas { height: 100%; width: 100%; margin: 0; padding: 0;}
+    </style>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?v=3.exp">
+    </script>
+    <script type="text/javascript">
+        
+        var map;
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+        function initialize() {
+            var mapOptions = {
+                zoom: 7,
+              panControl: false,
+              zoomControl: false,
+              mapTypeControl: false,
+              scaleControl: false,
+              streetViewControl: false,
+              overviewMapControl: false,
+              //38.8235622,42.2721864,7z
+            center: new google.maps.LatLng(38.8235622,42.2721864),
+            styles: 
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+                    // [{"featureType":"all","elementType":"all","stylers":[{"invert_lightness":true},{"hue":"#435158"},{"saturation":10},{"lightness":30},{"gamma":0.5}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels.text","stylers":[{"color":"#7c95de"},{"weight":0.69},{"saturation":95},{"lightness":20}]}]         
+                    
+                    [{"featureType":"all","elementType":"all","stylers":[{"invert_lightness":true},{"hue":"#435158"},{"saturation":10},{"lightness":30},{"gamma":0.5}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels.text","stylers":[{"visibility":"off"},{"color":"#7c95de"},{"weight":0.69},{"saturation":95},{"lightness":20}]},{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#000000"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]}]                           
+            };
+            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        }
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
+        google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+  </head>
+  <body>
+    <div id="map-canvas"></div>
+  </body>
 </html>
+                        
